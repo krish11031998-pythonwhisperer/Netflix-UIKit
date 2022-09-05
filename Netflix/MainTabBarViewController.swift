6a83ca606e92af1986c91abecd03f0ef74dd4b40
Krishna Venkatramani (Rain Work GPGPassphrase) <krishna@rain.bh>
2022-09-05 09:37:31 +0400

    Removed Downloads VC

--- Netflix/MainTabBarViewController.swift
+++ Netflix/MainTabBarViewController.swift
@@ -11,19 +11,18 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemYellow
+        view.backgroundColor = .systemYellow
         let navViews = [self.InitializeTabViewController(tab:.home),
-        self.InitializeTabViewController(tab: .downloads),
-        self.InitializeTabViewController(tab: .search),
-        self.InitializeTabViewController(tab: .upcoming)]
-        self.setViewControllers(navViews, animated: true)
-        self.tabBar.tintColor = .label
-        self.tabBar.layer.cornerRadius = 10;
-        self.tabBar.layer.backgroundColor = .init(red: 255, green: 255, blue: 255, alpha: 0.5)
+        InitializeTabViewController(tab: .search),
+		InitializeTabViewController(tab: .upcoming)].compactMap{ $0 }
+        setViewControllers(navViews, animated: true)
+        tabBar.tintColor = .label
+        tabBar.layer.cornerRadius = 10;
+        tabBar.layer.backgroundColor = .init(red: 255, green: 255, blue: 255, alpha: 0.5)
     }
     
-    func InitializeTabViewController(tab:TabBarModel) -> UINavigationController{
-        var vc:UIViewController
+    func InitializeTabViewController(tab:TabBarModel) -> UINavigationController? {
+        var vc:UIViewController? = nil
         switch(tab){
             case .home:
                 vc = HomeViewController()
@@ -31,11 +30,13 @@ class MainTabBarViewController: UITabBarController {
                 vc = SearchViewController()
             case .upcoming:
                 vc = UpcomingViewController()
-            case .downloads:
-                vc = DownloadsViewController()
+			default:
+				break
         }
-        
-        let navView = UINavigationController(rootViewController: vc)
+	
+		guard let validVC = vc else { return nil }
+		
+        let navView = UINavigationController(rootViewController: validVC)
         navView.tabBarItem = .init(title: tab.TabBarItemStyling.title, image: .init(systemName: tab.TabBarItemStyling.icon), tag: 1)
         return navView
         
